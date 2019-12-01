2fa97d5f8fc2e29506c24d4d1efa29ad3079736b
MatteoC <matteo.crc@gmail.com>
2019-12-01 18:22:16 +0100

    Update javascript_requirements.php

--- views/main/tmpl/javascript_requirements.php
+++ views/main/tmpl/javascript_requirements.php
@@ -1,6 +1,9 @@
 <link rel="stylesheet" href="components/com_conf_robot/views/main/tmpl/css/custom.css">
+<!-- include this if your website doesn't already have it -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-
+<!-- include this if your website doesn't already have it -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script type="text/javascript" src="components/com_prova/js/jquery.cookie.min.js"></script>
 <script type="text/javascript" src="components/com_prova/js/jquery.tablesorter.js"></script>
 <script type="text/javascript" src="components/com_conf_robot/js/conf_robot.js"></script>
