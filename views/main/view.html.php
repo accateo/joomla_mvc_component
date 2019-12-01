056f190e7a7b2d4ca9c9a4ff8823dedcc035128a
MatteoC <matteo.crc@gmail.com>
2019-12-01 18:12:22 +0100

    Add files via upload

---
+++ views/main/view.html.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Prova View for Com_Configurator Component
+ * 
+ *
+ * Created with Marco's Component Creator for Joomla! 1.6
+ * http://www.mmleoni.net/joomla-component-builder
+ *
+ */
+jimport( 'joomla.application.component.view');
+jimport('joomla.application.component.model');
+
+/**
+ * HTML View class for the Prova Component
+ *
+ * @package	Joomla.Components
+ * @subpackage	Prova
+ */
+class conf_configuratorViewmain extends JViewLegacy{
+	
+	function display($tpl = null)
+	{
+		// Assign data to the view         
+		
+		// Display the view
+		parent::display($tpl);
+		
+	}
+	
+	
+}
+?>
